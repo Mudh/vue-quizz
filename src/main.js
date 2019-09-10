53040637ba3a5aa3f2a3840f8ecc0a4b137e397f
John Berry <jonmudh@gmail.com>
2019-09-10 16:25:14 +0200

    Set up axios with firebase

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
 import Vue from 'vue';
+import axios from 'axios';
+
 import App from './App.vue';
 import router from './router';
-import store from './store';
+import store from './store/index';
 
 Vue.config.productionTip = false;
+
+axios.defaults.baseURL = 'https://vue-quizz-be217.firebaseio.com/';
+axios.defaults.headers.get.Accepts = 'application/json';
 
 new Vue({
   router,
