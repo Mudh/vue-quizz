2b85c050134d976195f8dec9921d3c2d13c3728d
John Berry <jonmudh@gmail.com>
2019-09-10 14:47:05 +0200

    CTA signup routing

--- src/components/HomeVisitor.vue
+++ src/components/HomeVisitor.vue
@@ -6,6 +6,7 @@
       It's like a personal "todolist" training project transpose
       <br />into a real playable quizz, try it now !
     </p>
+    <button @click="subscribeQuiz">Sign Up</button>
   </div>
 </template>
 
@@ -14,12 +15,19 @@ export default {
   name: 'HomeVisitor',
   props: {
     msg: String
+  },
+  methods: {
+    subscribeQuiz() {
+      this.$router.push('signup');
+    }
   }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+@import '../styles/variables.scss';
+
 .welcome {
   margin-top: 15%;
 }
