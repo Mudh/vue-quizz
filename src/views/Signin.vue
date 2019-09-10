1b02af4c0649a9276902f36a060598a8817e165a
John Berry <jonmudh@gmail.com>
2019-09-10 14:49:10 +0200

    Sign up and in views

---
+++ src/views/Signin.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="signin">
+    <form @submit.prevent="onSubmit">
+      <h1>Sign In</h1>
+      <div class="input">
+        <label for="email">Mail</label>
+        <input type="email" id="email" v-model="email" />
+      </div>
+      <div class="input">
+        <label for="password">Password</label>
+        <input type="password" id="password" v-model="password" />
+      </div>
+      <div class="submit">
+        <button type="submit">Submit</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: '',
+      password: ''
+    };
+  },
+  methods: {
+    onSubmit() {
+      const formData = {
+        email: this.email,
+        password: this.password
+      };
+      console.log('signin data', formData);
+      this.$store.dispatch('login', {
+        email: formData.email,
+        password: formData.password
+      });
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import '../styles/form.scss';
+</style>
---
+++ src/views/Signup.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="signup">
+    <form @submit.prevent="onSubmit">
+      <h1>Sign Up</h1>
+      <div class="input">
+        <label for="nickname">Nickname</label>
+        <input type="text" id="nickname" v-model="nickname" />
+      </div>
+      <div class="input">
+        <label for="email">Mail</label>
+        <input type="email" id="email" v-model="email" />
+      </div>
+      <div class="input">
+        <label for="password">Password</label>
+        <input type="password" id="password" v-model="password" />
+      </div>
+      <div class="input">
+        <label for="confirm-password">Confirm Password</label>
+        <input type="password" id="confirm-password" v-model="confirmPassword" />
+      </div>
+      <div class="submit">
+        <button type="submit">Submit</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      nickname: '',
+      email: '',
+      password: '',
+      confirmPassword: ''
+    };
+  },
+  methods: {
+    onSubmit() {
+      const formData = {
+        nickname: this.nickname,
+        email: this.email,
+        password: this.password,
+        confirmPassword: this.confirmPassword
+      };
+      console.log('sign up data', formData);
+      // this.$store.dispatch('signup', formData);
+    }
+  }
+};
+</script>
+
+<style lang='scss'>
+@import '../styles/variables.scss';
+@import '../styles/form.scss';
+</style>
