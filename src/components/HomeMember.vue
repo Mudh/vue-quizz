9b6c99ad2573ef91cd5341f3db4d92769cada1b5
John Berry <jonmudh@gmail.com>
2019-09-11 14:14:58 +0200

    Add HomeMember view on login

---
+++ src/components/HomeMember.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="welcome">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <h1>{{ msg }}</h1>
+    <button @click="subscribeQuiz">Start</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeMember',
+  props: {
+    msg: String
+  },
+  methods: {
+    subscribeQuiz() {
+      this.$router.push('signup');
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+@import '../styles/variables.scss';
+
+.welcome {
+  margin-top: 15%;
+}
+</style>
