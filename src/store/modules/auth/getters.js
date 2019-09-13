d835c8f1c38477096957ed2ed6eb7eaea6b8a4fc
John Berry <jonmudh@gmail.com>
2019-09-13 15:08:52 +0200

    Fix async issue returning null

--- src/store/modules/auth/getters.js
+++ src/store/modules/auth/getters.js
@@ -1,4 +1,8 @@
-const user = state => state.user;
+const user = (state) => {
+  if (!state.user) {
+    return false;
+  } return state.user;
+};
 const isAuthenticated = state => state.idToken !== null;
 
 export default {
