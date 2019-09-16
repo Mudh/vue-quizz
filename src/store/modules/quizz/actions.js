0ab5c5e5cf3846a70e9e71c97bce1abd5f6de9a5
John Berry <jonmudh@gmail.com>
2019-09-16 18:45:40 +0200

    Quizz start + questions/answers fetch

--- src/store/modules/quizz/actions.js
+++ src/store/modules/quizz/actions.js
@@ -1,11 +1,31 @@
-// import axios from 'axios';
+import axios from 'axios';
 
-// QUIZZ STEPS //////////////////////////////////////
+// QUIZZ HANDLERS //////////////////////////////////////
 
-const startQuizz = ({ commit }) => {
+const startQuizz = ({ commit, dispatch }) => {
   commit('startQuizz');
+  dispatch('fetchQuestions');
+};
+
+const nextQuestion = ({ commit }) => {
+  commit('nextQuestion');
+};
+
+// FETCH QUESTIONS //////////////////////////////////////
+
+const fetchQuestions = ({ rootState, state }) => {
+  if (!rootState.auth.idToken) {
+    return;
+  }
+  axios
+    .get(`/questionsList.json?auth=${rootState.auth.idToken}`)
+    .then((res) => {
+      state.quizzQuestions = res.data;
+      console.log('created', res.data);
+    })
+    .catch(error => console.log(error));
 };
 
 export default {
-  startQuizz,
+  startQuizz, nextQuestion, fetchQuestions
 };
