20a01036996d4ef0452d184528b18e3da06829d6
John Berry <jonmudh@gmail.com>
2019-09-30 13:11:41 +0200

    user points getter

--- src/store/modules/auth/getters.js
+++ src/store/modules/auth/getters.js
@@ -4,8 +4,14 @@ const user = (state) => {
   } return state.user;
 };
 const isAuthenticated = state => state.idToken !== null;
+const userPoints = (state) => {
+  if (!state.user) {
+    return false;
+  } return state.user.nb_points;
+};
 
 export default {
   user,
   isAuthenticated,
+  userPoints,
 };
